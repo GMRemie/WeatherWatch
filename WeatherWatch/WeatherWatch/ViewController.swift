fcddecacb13bf07442ea78704552e1e648a1a344
Joseph <gmremcorat@gmail.com>
2019-05-21 13:12:10 -0400

    Initial xcode commit
    Initial xcode files

---
+++ WeatherWatch/WeatherWatch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherWatch
+//
+//  Created by Joseph Storer on 5/21/19.
+//  Copyright © 2019 Joseph Storer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
